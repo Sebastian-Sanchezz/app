f0ef5db504558b4cc8d226c5a7c10cb6c3617aad
scsebastian1406 <scsebastian1406@gmail.com>
2024-06-25 20:39:05 +0000

    avance1

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+
+from flask import Flask, send_file, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('usuario/index.html')
+
+@app.get("/registro-saco")
+def get_registro_saco():
+    return render_template('usuario/registro-saco.html')
+
+@app.get("/registro-bolsa")
+def get_registro_bolsa():
+    return render_template('usuario/registro-bolsa.html')
+
+def main():
+    app.run(port=int(os.environ.get('PORT', 80)))
+
+if __name__ == "__main__":
+    main()
